b8bbfff7ca896a421675728dac98c6092364dd53
TristanDend <tdend413@gmail.com>
2025-03-09 18:45:14 -0400

    Completed all components and passed all tests.

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,98 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function ChangeColor(): React.JSX.Element {
+    const [colorSelected, setColor] = useState<string>("");
+
+    function changeColor(event: React.ChangeEvent<HTMLInputElement>) {
+        setColor(event.target.value);
+    }
+
     return (
         <div>
             <h3>Change Color</h3>
+            <Form.Check
+                inline
+                type="radio"
+                onChange={changeColor}
+                label="Red"
+                value="red"
+                style={{ backgroundColor: "red" }}
+                checked={colorSelected === "red"}
+            />
+            <Form.Check
+                inline
+                type="radio"
+                onChange={changeColor}
+                label="Orange"
+                value="orange"
+                style={{ backgroundColor: "orange" }}
+                checked={colorSelected === "orange"}
+            />
+            <Form.Check
+                inline
+                type="radio"
+                onChange={changeColor}
+                label="Yellow"
+                value="yellow"
+                style={{ backgroundColor: "yellow" }}
+                checked={colorSelected === "yellow"}
+            />
+            <Form.Check
+                inline
+                type="radio"
+                onChange={changeColor}
+                label="Green"
+                value="green"
+                style={{ backgroundColor: "green" }}
+                checked={colorSelected === "green"}
+            />
+            <Form.Check
+                inline
+                type="radio"
+                onChange={changeColor}
+                label="Blue"
+                value="blue"
+                style={{ backgroundColor: "blue" }}
+                checked={colorSelected === "blue"}
+            />
+            <Form.Check
+                inline
+                type="radio"
+                onChange={changeColor}
+                label="Purple"
+                value="purple"
+                style={{ backgroundColor: "purple" }}
+                checked={colorSelected === "purple"}
+            />
+            <Form.Check
+                inline
+                type="radio"
+                onChange={changeColor}
+                label="White"
+                value="white"
+                style={{ backgroundColor: "white" }}
+                checked={colorSelected === "white"}
+            />
+            <Form.Check
+                inline
+                type="radio"
+                onChange={changeColor}
+                label="Black"
+                value="black"
+                style={{ backgroundColor: "black" }}
+                checked={colorSelected === "black"}
+            />
+            <div
+                data-testid="colored-box"
+                style={{ backgroundColor: colorSelected }}
+            >
+                You have chosen{" "}
+                <text style={{ backgroundColor: colorSelected }}>
+                    {colorSelected}
+                </text>
+                .
+            </div>
         </div>
     );
 }
--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,52 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): React.JSX.Element {
+    const [isEditing, setEditMode] = useState<boolean>(false);
+    const [userName, setUserName] = useState<string>("Your Name");
+    const [isStudent, setStudentState] = useState<boolean>(true);
+
+    function updateEditMode(event: React.ChangeEvent<HTMLInputElement>) {
+        setEditMode(event.target.checked);
+    }
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                checked={isEditing}
+                onChange={updateEditMode}
+            ></Form.Check>
+            <p>
+                {isEditing ? (
+                    <Form.Group>
+                        <Form.Label>
+                            Change username:
+                            <Form.Control
+                                value={userName}
+                                onChange={(event) => {
+                                    setUserName(event.target.value);
+                                }}
+                            ></Form.Control>
+                        </Form.Label>
+                        <Form.Label>
+                            Change student state:
+                            <Form.Check
+                                label="Are you a student?"
+                                checked={isStudent}
+                                onChange={(event) => {
+                                    setStudentState(event.target.checked);
+                                }}
+                            ></Form.Check>
+                        </Form.Label>
+                    </Form.Group>
+                ) : (
+                    <p>
+                        {userName} is {!isStudent ? "not" : ""} a student
+                    </p>
+                )}
+            </p>
         </div>
     );
 }
